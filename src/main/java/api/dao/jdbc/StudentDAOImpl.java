7e10d16c5e8e82bc08a046546267f70e38c0c680
collins <collinsboniface1000@gmail.com>
2022-01-03 10:06:49 +0200

    Fixed the bug in the program whereby the Exception was a null exception so to fix that i need to i need to pass a datasource into the jdbcTemplate whereby the i create the getdatasource method to return the Datasource and pass into the jdbcTemplate constructor

--- src/main/java/api/dao/jdbc/StudentDAOImpl.java
+++ src/main/java/api/dao/jdbc/StudentDAOImpl.java
@@ -2,17 +2,33 @@ package api.dao.jdbc;
 
 import com.jdbc.api.Student;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 public class StudentDAOImpl implements StudentDAO {
 
-    private JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate(getDataSource());
 
     @Override
     public void insert(Student student) {
         // The logic to store the student object into the database
         String sql = "INSERT INTO student VALUES (?, ?, ?)";
         Object[] arg = {student.getRollNo(),student.getName(),student.getAddress()};
-        jdbcTemplate.update(sql, arg);
+
+        int noOfRowInserted = jdbcTemplate.update(sql, arg);
+        System.out.println("No of row inserted is: " + noOfRowInserted);
+
+    }
+
+    public DataSource getDataSource(){
+        String url = "jdbc:postgresql://localhost:5432/school";
+        String username = "";
+        String password = "";
+
+        DataSource dataSource = new DriverManagerDataSource(url, username, password);
+
+        return dataSource;
     }
 
 }
