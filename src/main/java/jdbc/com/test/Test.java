92b28bb878ef52e1f52792812894e5404d6ba1f0
collins <collinsboniface1000@gmail.com>
2022-01-03 08:41:10 +0200

    Created a student object

--- src/main/java/jdbc/com/test/Test.java
+++ src/main/java/jdbc/com/test/Test.java
@@ -1,7 +1,12 @@
 package jdbc.com.test;
 
+import com.jdbc.api.Student;
+
 public class Test {
     public static void main(String[] args) {
-        
+        Student newStudent1 = new Student();
+        newStudent1.setRollNo(001);
+        newStudent1.setName("Collins");
+        newStudent1.setAddress("Sakina");
     }
 }
