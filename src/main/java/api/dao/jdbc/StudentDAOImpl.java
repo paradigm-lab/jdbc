7c0cafd6d2239d9a9eb5cfb2d920922a36975bec
collins <collinsboniface1000@gmail.com>
2022-01-03 09:31:22 +0200

    Created a studentDAOImpl class that implement the StudentDAO interface with one method in the interface that it need to be implemented in the StudentDAOImpl class

---
+++ src/main/java/api/dao/jdbc/StudentDAOImpl.java
@@ -0,0 +1,16 @@
+package api.dao.jdbc;
+
+import com.jdbc.api.Student;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class StudentDAOImpl implements StudentDAO {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void insert(Student student) {
+        // The logic to store the student object into the database
+
+    }
+
+}
