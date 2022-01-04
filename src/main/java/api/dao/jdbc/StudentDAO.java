217536b3cfb891503307092cd347bb0f41e1f5d0
collins <collinsboniface1000@gmail.com>
2022-01-04 11:12:08 +0200

    Added a new delete method into the inteface which takes two arguments which is the studentName and studentAddress

--- src/main/java/api/dao/jdbc/StudentDAO.java
+++ src/main/java/api/dao/jdbc/StudentDAO.java
@@ -5,4 +5,5 @@ import com.jdbc.api.Student;
 public interface StudentDAO {
     void insert(Student student);
     boolean deleteRecordByRollNo(int roolNo);
+    int deleteRecordByStudentNameAndStudentAddress(String studentName, String studentAddress);
 }
