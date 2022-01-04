46a94d4e9c55793c88dce2f0245f8829183697b0
collins <collinsboniface1000@gmail.com>
2022-01-04 08:44:16 +0200

    Load the xml file and settind up the log if the xml file is loaded

--- src/main/java/jdbc/com/test/Test.java
+++ src/main/java/jdbc/com/test/Test.java
@@ -3,15 +3,25 @@ package jdbc.com.test;
 import api.dao.jdbc.StudentDAO;
 import api.dao.jdbc.StudentDAOImpl;
 import com.jdbc.api.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        Student newStudent1 = new Student();
-        newStudent1.setRollNo(003);
-        newStudent1.setName("Abhishek");
-        newStudent1.setAddress("Blr");
 
-        StudentDAO studentDAO = new StudentDAOImpl();
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("Application context loaded....");
+        /*
+        StudentDAOImpl studentDAO = context.getBean("studentDao", StudentDAOImpl.class);
+
+
+        Student newStudent1 = new Student();
+        newStudent1.setRollNo(004);
+        newStudent1.setName("Rose");
+        newStudent1.setAddress("Dodoma");
+
         studentDAO.insert(newStudent1);
+
+         */
     }
 }
