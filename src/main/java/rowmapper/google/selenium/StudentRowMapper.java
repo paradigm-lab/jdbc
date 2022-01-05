9454d686fb123dd4e90fb29485374e8f644e6874
collins <collinsboniface1000@gmail.com>
2022-01-05 08:33:16 +0200

    Created a new package and the new class which is the rowMapper class where by the class implement the RowMapper interface and override the mapRow method which return an student

--- src/main/java/rowmapper/google/selenium/StudentRowMapper.java
+++ src/main/java/rowmapper/google/selenium/StudentRowMapper.java
@@ -1,2 +1,22 @@
-package rowmapper.google.selenium;public class StudentRowMapper {
+package rowmapper.google.selenium;
+
+import com.jdbc.api.Student;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentRowMapper implements RowMapper<Student> {
+
+    @Override
+    public Student mapRow(ResultSet res, int rowNum) throws SQLException {
+
+        Student newStudent = new Student();
+
+        newStudent.setRollNo(res.getInt("roll_no"));
+        newStudent.setName(res.getString("student_name"));
+        newStudent.setAddress(res.getString("student_addres"));
+
+        return newStudent;
+    }
 }
