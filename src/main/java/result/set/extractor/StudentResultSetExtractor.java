3a3e1d86172200be6e1515c99c1e2c45173d3487
collins <collinsboniface1000@gmail.com>
2022-01-06 13:25:03 +0200

    Adding the implementation of the extractData method after override the method from the ResultSetExtractor interface

--- src/main/java/result/set/extractor/StudentResultSetExtractor.java
+++ src/main/java/result/set/extractor/StudentResultSetExtractor.java
@@ -1,4 +1,32 @@
 package result.set.extractor;
 
-public class StudentResultSetExtractor {
+import com.jdbc.api.Student;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentResultSetExtractor implements ResultSetExtractor<List<Student>> {
+
+
+    @Override
+    public List<Student> extractData(ResultSet rs) throws SQLException, DataAccessException {
+
+        List<Student> studentList = new ArrayList<Student>();
+        while(rs.next()) {
+            Student student = new Student();
+
+            student.setRollNo(rs.getInt("roll_no"));
+            student.setName(rs.getString("student_name"));
+            student.setAddress(rs.getString("student_addres"));
+
+            studentList.add(student);
+        }
+
+        return studentList;
+    }
 }
+
