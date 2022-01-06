410deb911f05359f077aee77b3215b9f0f50a58c
collins <collinsboniface1000@gmail.com>
2022-01-06 16:49:39 +0200

    Implement the groupStudentByResultSetExtractor with the ResultSetExtractor interface whereby we are going to implement the extractData method from the interface

--- src/main/java/result/set/extractor/groupStudentByResultSetExtractor.java
+++ src/main/java/result/set/extractor/groupStudentByResultSetExtractor.java
@@ -6,14 +6,35 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class groupStudentByResultSetExtractor implements ResultSetExtractor<Map<String, List<Student>>> {
+public class groupStudentByResultSetExtractor implements ResultSetExtractor<Map<String, List<String>>> {
 
     @Override
-    public Map<String, List<Student>> extractData(ResultSet rs) throws SQLException, DataAccessException {
+    public Map<String, List<String>> extractData(ResultSet res) throws SQLException, DataAccessException {
 
-        return null;
+        Map<String, List<String>> studentTable = new HashMap<>();
+
+        while (res.next()) {
+            String studentName = res.getString("student_addres");
+            String studentAddress = res.getString("student_name");
+
+            List<String> studentNameList = studentTable.get(studentAddress);
+
+            if (studentNameList == null) {
+                ArrayList<String> newStudentList = new ArrayList<>();
+                newStudentList.add(studentName);
+
+                studentTable.put(studentAddress, newStudentList);
+            }
+            else {
+                studentNameList.add(studentName);
+            }
+        }
+
+        return studentTable;
     }
 }
