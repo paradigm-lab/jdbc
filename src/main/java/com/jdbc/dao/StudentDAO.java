737df26b16cd90beb863d204e7afcb28b1719fca
collins <collinsboniface1000@gmail.com>
2022-03-23 20:13:16 +0300

    Added a method in an interface for the batch insert

--- src/main/java/com/jdbc/dao/StudentDAO.java
+++ src/main/java/com/jdbc/dao/StudentDAO.java
@@ -2,12 +2,13 @@ package com.jdbc.dao;
 
 import com.jdbc.api.Student;
 
+import java.util.List;
+
 
 // CRUD operations methods
 public interface StudentDAO {
-
     void insert(Student student);
     boolean deleteRecordByRollNo(int rollNo);
     int deleteRecordByStudentNameOrStudentAddress(String studentName, String studentAddress);
-
+    void insert(List<Student> students);
 }
