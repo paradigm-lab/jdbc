6f194a1f390fa2c903c40684261c267f2cfdd7e9
collins <collinsboniface1000@gmail.com>
2022-03-23 19:52:02 +0300

    Created a DAO helper class with the method to set the student objects in the database

---
+++ src/main/java/com/jdbc/service/StudentDAOHelper.java
@@ -0,0 +1,42 @@
+package com.jdbc.service;
+
+import com.jdbc.api.Student;
+
+public class StudentDAOHelper {
+
+    public void setUpStudentTable() {
+        Student student = new Student();
+        student.setRollNo(001);
+        student.setName("Collin");
+        student.setAddress("Sakina");
+
+        Student student1 = new Student();
+        student1.setRollNo(003);
+        student1.setName("Kangaroo");
+        student1.setAddress("Pune");
+
+        Student student2 = new Student();
+        student2.setRollNo(004);
+        student2.setName("David");
+        student2.setAddress("Pune");
+
+
+        Student student3 = new Student();
+        student3.setRollNo(005);
+        student3.setName("Jon");
+        student3.setAddress("New York");
+
+
+        Student student4 = new Student();
+        student4.setRollNo(006);
+        student4.setName("Prasad");
+        student4.setAddress("Pune");
+
+        Student student5 = new Student();
+        student5.setRollNo(002);
+        student5.setName("Noura");
+        student5.setAddress("Austria");
+    }
+
+}
+
