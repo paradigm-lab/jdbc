1bcc90759aab693a77a837c438ee2c4b56fbc9ce
collins <collinsboniface1000@gmail.com>
2022-03-23 08:14:24 +0300

    Created a student object and used the insert method to insert the object into the database

--- src/main/java/com/jdbc/test/Test.java
+++ src/main/java/com/jdbc/test/Test.java
@@ -1,6 +1,8 @@
 package com.jdbc.test;
 
 import com.jdbc.api.Student;
+import com.jdbc.dao.StudentDAO;
+import com.jdbc.dao.StudentDAOImpl;
 
 public class Test {
 
@@ -13,7 +15,14 @@ public class Test {
         student.setName("Collin");
         student.setAddress("Sakina");
 
+        Student student1 = new Student();
+        student1.setRollNo(002);
+        student1.setName("Paradigm");
+        student1.setAddress("Anonymous");
 
+        StudentDAO studentDAO = new StudentDAOImpl();
+        studentDAO.insert(student);
+        studentDAO.insert(student1);
     }
 
 }
