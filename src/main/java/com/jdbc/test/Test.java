696bac0e7ba6b94b693820e0eb9f6ea3db307606
collins <collinsboniface1000@gmail.com>
2022-03-23 18:49:30 +0300

    Inserted more data in the table

--- src/main/java/com/jdbc/test/Test.java
+++ src/main/java/com/jdbc/test/Test.java
@@ -14,27 +14,54 @@ public class Test {
 
         StudentDAOImpl studentDAO = context.getBean("studentDao", StudentDAOImpl.class);
 
+        /*
         // Creating Student class object
         Student student = new Student();
-
-        /*
         student.setRollNo(001);
         student.setName("Collin");
         student.setAddress("Sakina");
 
         Student student1 = new Student();
-        student1.setRollNo(002);
-        student1.setName("Paradigm");
-        student1.setAddress("Anonymous");
+        student1.setRollNo(003);
+        student1.setName("Collin");
+        student1.setAddress("Pune");
+
+        Student student2 = new Student();
+        student2.setRollNo(004);
+        student2.setName("David");
+        student2.setAddress("Pune");
+
+
+        Student student3 = new Student();
+        student3.setRollNo(005);
+        student3.setName("Rakesh");
+        student3.setAddress("Odisha");
+
+
+        Student student4 = new Student();
+        student4.setRollNo(006);
+        student4.setName("Prasad");
+        student4.setAddress("Pune");
+
+        Student student5 = new Student();
+        student5.setRollNo(002);
+        student5.setName("Collin");
+        student5.setAddress("Anonymous");
 
         studentDAO.insert(student);
         studentDAO.insert(student1);
+        studentDAO.insert(student2);
+        studentDAO.insert(student3);
+        studentDAO.insert(student4);
+        studentDAO.insert(student5);
         */
 
+        /*
         boolean doneUpdate = studentDAO.deleteRecordByRollNo(1);
-        if (doneUpdate){
+        if (doneUpdate) {
             System.out.println("Record number 1 is deleted");
         }
+        */
 
     }
 
