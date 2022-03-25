07eddced26689cdf61ec6382d86964d82e730be2
collins <collinsboniface1000@gmail.com>
2022-03-25 03:12:41 +0300

    Added the find all Student method in the interface so that it can be implemented in the imple class

--- src/main/java/com/jdbc/dao/StudentDAO.java
+++ src/main/java/com/jdbc/dao/StudentDAO.java
@@ -7,8 +7,15 @@ import java.util.List;
 
 // CRUD operations methods
 public interface StudentDAO {
+
     void insert(Student student);
+
     boolean deleteRecordByRollNo(int rollNo);
+
     int deleteRecordByStudentNameOrStudentAddress(String studentName, String studentAddress);
+
     void insert(List<Student> students);
+
+    List<Student> findAllStudent();
+
 }
